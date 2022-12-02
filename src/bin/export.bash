c3479b1be30087f044fe29c910bfb57738e777e6
Tim Oram <dev@mitmaro.ca>
2022-12-02 16:23:28 -0330

    Add exported data

---
+++ src/bin/export.bash
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+commits="$(git --git-dir "$1.git" log --pretty=format:"%h" --follow -- _data/"$2".yml)"
+
+export_dir="$PWD"/exported/
+
+mkdir -p "$export_dir"
+
+pushd $1
+for commit in $commits; do
+	file="$(git show -s --format=%cs-%ct-"$commit" "$commit")-$2.yml"
+	git show "$commit":"_data/$2.yml" > "$export_dir"/"$file"
+done
+popd
