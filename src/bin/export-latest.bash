49918279c1b5477c9e9b6b0fbd2abe81b65724f5
Tim Oram <dev@mitmaro.ca>
2022-12-23 11:40:42 -0330

    Scripts and exported data update

--- src/bin/export-latest.bash
+++ src/bin/export-latest.bash
@@ -8,9 +8,5 @@ mkdir -p "$export_dir"
 
 pushd "$1"
 file="$(git show -s --format=%cs-%ct-%h)-$2.yml"
-echo $file
 git show :"_data/$2.yml" > "$export_dir"/"$file"
-
 popd
-
-./src/bin/cts.js "$2" "$export_dir"/"$file" "$3"
